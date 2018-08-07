277e4d3980310f58dbb35c072fc2c84406de9c0a
Jacinto Arias <arias.jacinto@gmail.com>
2018-08-07 13:01:44 +0200

    removing flask response from callbacks

--- dash/serverless.py
+++ dash/serverless.py
@@ -6,6 +6,61 @@ import plotly
 
 class DashServerless(dash.Dash):
 
+  # TODO - Update nomenclature.
+  # "Parents" and "Children" should refer to the DOM tree
+  # and not the dependency tree.
+  # The dependency tree should use the nomenclature
+  # "observer" and "controller".
+  # "observers" listen for changes from their "controllers". For example,
+  # if a graph depends on a dropdown, the graph is the "observer" and the
+  # dropdown is a "controller". In this case the graph's "dependency" is
+  # the dropdown.
+  # TODO - Check this map for recursive or other ill-defined non-tree
+  # relationships
+  # pylint: disable=dangerous-default-value
+  def callback(self, output, inputs=[], state=[], events=[]):
+      self._validate_callback(output, inputs, state, events)
+
+      callback_id = '{}.{}'.format(
+          output.component_id, output.component_property
+      )
+      self.callback_map[callback_id] = {
+          'inputs': [
+              {'id': c.component_id, 'property': c.component_property}
+              for c in inputs
+          ],
+          'state': [
+              {'id': c.component_id, 'property': c.component_property}
+              for c in state
+          ],
+          'events': [
+              {'id': c.component_id, 'event': c.component_event}
+              for c in events
+          ]
+      }
+
+      def wrap_func(func):
+          @wraps(func)
+          def add_context(*args, **kwargs):
+
+              output_value = func(*args, **kwargs)
+              response = {
+                  'response': {
+                      'props': {
+                          output.component_property: output_value
+                      }
+                  }
+              }
+
+              return json.dumps(response, cls=plotly.utils.PlotlyJSONEncoder)
+
+          self.callback_map[callback_id]['callback'] = add_context
+
+          return add_context
+
+      return wrap_func
+
+
   def index_handler(self):
     return self.index()
 
@@ -47,4 +102,4 @@ class DashServerless(dash.Dash):
             c['id'] == component_registration['id']
         ][0])
 
-    return json.dumps(self.callback_map[target_id]['callback'](*args), cls=plotly.utils.PlotlyJSONEncoder)
+    return self.callback_map[target_id]['callback'](*args), cls=plotly.utils.PlotlyJSONEncoder)
